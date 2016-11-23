f330bfbc3922cc166c46cf04be01c857725e1e0e
Jacob Keen <jaclkeen@gmail.com>
2016-11-23 13:09:30 -0600

    finished user model

---
+++ src/FriendBook/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FriendBook.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string email { get; set; }
+
+        [Required]
+        public string password { get; set; }
+
+        public string profileImg { get; set; }
+    }
+}
