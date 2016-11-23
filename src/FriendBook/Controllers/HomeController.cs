09b5ce225480046c0d504d578d68532eea0f8c16
Jacob Keen <jaclkeen@gmail.com>
2016-11-23 17:19:06 -0600

    able to now load each status and user name to the view

--- src/FriendBook/Controllers/HomeController.cs
+++ src/FriendBook/Controllers/HomeController.cs
@@ -3,14 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using FriendBook.Data;
+using FriendBook.ViewModels;
+using FriendBook.Models;
 
 namespace FriendBook.Controllers
 {
     public class HomeController : Controller
     {
+        private FriendBookContext context;
+
+        public HomeController(FriendBookContext ctx)
+        {
+            context = ctx;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var posts = context.Post.ToList();
+            var users = context.User.ToList();
+
+            foreach(Post p in posts)
+            {
+                foreach(User u in users)
+                {
+                    if(p.UserId == u.UserId)
+                    {
+                        p.User = u;
+                    }
+                }
+            }
+
+            HomePageViewModel model = new HomePageViewModel();
+            model.Posts = posts;
+
+            return View(model);
+        }
+
+        public IActionResult NewStatus(Post post)
+        {
+            return RedirectToAction("Index");
         }
 
         public IActionResult About()
