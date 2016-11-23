db736d9c6366406aa800854961890a1ce845b59b
Jacob Keen <jaclkeen@gmail.com>
2016-11-23 14:02:31 -0600

    small modifications to other models, and created the relationship model

---
+++ src/FriendBook/Models/Relationship.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FriendBook.Models
+{
+    public class Relationship
+    {
+        [Key]
+        public int RelationshipId { get; set; }
+
+        [Required]
+        public int UserId1 { get; set; }
+        public User User1 { get; set; }
+
+        [Required]
+        public int UserId2 { get; set; }
+        public User User2 { get; set; }
+
+        [Required]
+        public int Status { get; set; }
+
+        //Status Int Meanings:
+        //    0: Pending
+        //    1: Accepted
+        //    2: Declined
+        //    3: Blocked
+    }
+}
