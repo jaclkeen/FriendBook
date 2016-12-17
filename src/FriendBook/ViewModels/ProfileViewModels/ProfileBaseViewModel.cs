429f68cb731214442c62eb6258712eaa2e3b2803
Jacob Keen <jaclkeen@gmail.com>
2016-12-17 16:34:45 -0600

    fixed friend status on a users profile view bug

--- src/FriendBook/ViewModels/ProfileViewModels/ProfileBaseViewModel.cs
+++ src/FriendBook/ViewModels/ProfileViewModels/ProfileBaseViewModel.cs
@@ -22,8 +22,10 @@ namespace FriendBook.ViewModels
             //WHEN A NEW INSTANCE OF PROFILEBASEVIEWMODEL IS CREATED, IT GETS THE CURRENT USER PROFILES ID AND SETS THE
             //USER PROFILE EQUAL TO THAT USER, GETS THE STYLE OF THAT PARTICULAR USER, QUERIES THROUGH ALL RELATIONSHIPS
             //TO FIND THE TYPE OF RELATIONSHIP SHARED BETWEEN THE CURRENT LOGGED IN USER, AND THE PROFILE BEING VIEWED
-            //
 
+            User LoggedInUser = ActiveUser.Instance.User;
+
+            //THE USER WHOSE PROFILE IS BEING VISITED
             User user = ctx.User.Where(u => u.UserId == UserProfileId).SingleOrDefault();
             UserProfile = user;
 
@@ -32,35 +34,38 @@ namespace FriendBook.ViewModels
 
             List<Relationship> relationships = ctx.Relationship.Where(r => r.ReciverUserId == UserProfileId || r.SenderUserId == UserProfileId).ToList();
 
-            foreach (Relationship r in relationships)
+            if (relationships.Count > 0)
             {
-                if (r.SenderUserId == user.UserId || r.ReciverUserId == user.UserId)
+                foreach (Relationship r in relationships)
                 {
-                    if (r.Status == 0)
+                    if (r.SenderUserId == LoggedInUser.UserId || r.ReciverUserId == LoggedInUser.UserId)
                     {
-                        this.AreFriends = "Pending";
-                        break;
-                    }
-                    else if (r.Status == 1)
-                    {
-                        this.AreFriends = "yes";
-                        break;
-                    }
-                    else if (r.Status == 2)
-                    {
-                        this.AreFriends = "no";
-                        break;
-                    }
-                    else
-                    {
-                        this.AreFriends = "blocked";
+                        if (r.Status == 0)
+                        {
+                            AreFriends = "Pending";
+                            break;
+                        }
+                        else if (r.Status == 1)
+                        {
+                            AreFriends = "yes";
+                            break;
+                        }
+                        else if (r.Status == 2)
+                        {
+                            AreFriends = "no";
+                            break;
+                        }
+                        else
+                        {
+                            AreFriends = "blocked";
+                        }
                     }
                 }
-            }
 
-            if (this.AreFriends == null)
-            {
-                this.AreFriends = "NoRelationship";
+                if (AreFriends == null)
+                {
+                    AreFriends = "NoRelationship";
+                }
             }
         }
     }
