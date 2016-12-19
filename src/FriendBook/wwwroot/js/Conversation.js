3ee01d2d96a6ba8c4d6ec6d0e5fc9950efc475fa
Jacob Keen <jaclkeen@gmail.com>
2016-12-19 13:32:16 -0600

    beginning the styling of message boxes for future user messaging

---
+++ src/FriendBook/wwwroot/js/Conversation.js
@@ -0,0 +1,18 @@
+﻿$(".hideConversation").on("click", function () {
+    let conversation = $(this).parent().parent().parent()
+    conversation.addClass("minifiedConversation")
+    $(this).addClass("hidden")
+    $(this).siblings(".showConversation").removeClass("hidden")
+})
+
+$(".showConversation").on("click", function () {
+    let conversation = $(this).parent().parent().parent()
+    conversation.removeClass("minifiedConversation")
+    $(this).addClass("hidden")
+    $(this).siblings(".hideConversation").removeClass("hidden")
+})
+
+$(".removeConversation").on("click", function () {
+    let conversation = $(this).parent().parent().parent()
+    conversation.remove();
+})
